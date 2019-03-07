a07cbf6c8541f3749fca01439ee362a0a29a03ad
Aaron Magid <ahm64@case.edu>
2019-03-07 01:25:14 -0500

    feat(Websocket): Added RPC-like interface over websockets.

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 'use strict';
 const http = require('http');
+const request = require('request');
 
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-const socketIO = require('socket.io');
+const ws = require('ws');
 
 const config = require('../config').get();
 const logger = require('./services/logger');
@@ -16,10 +17,7 @@ setUpAPI();
 
 const server = http.Server(app);
 
-//Set up SocketIO server for realtime services
-const io = socketIO(server);
-
-setUpSocket();
+setUpSocket(new ws.Server(config.ws));
 
 server.listen(process.env.PORT || config.app.port);
 logger.info(`Server listening on port ${process.env.PORT || config.app.port}`);
@@ -39,13 +37,54 @@ function setUpAPI() {
 }
 
 
-function setUpSocket() {
-    io.on('connection', socket => {
+function setUpSocket(wsServer) {
+    wsServer.on('connection', socket => {
         console.log('Connection Received');
         socket.emit('connected', 'connected');
 
         socket.on('disconnect', () => {
             console.log('Connection Terminated');
+        });
+
+        const httpActions = {
+            user: {
+                getAll: 'get',
+                getById: 'get',
+                create: 'post',
+                update: 'patch',
+                delete: 'delete'
+            },
+            project: {
+                getAll: 'get',
+                getById: 'get',
+                getByUserId: 'get',
+                create: 'post',
+                update: 'patch',
+                delete: 'delete'
+            }
+        }
+
+        socket.on('message', message => {
+            message = JSON.parse(message);
+            if (message.entityType === 'user') {
+                return request({
+                    headers: {'content-type' : 'application/json'},
+                    url: 'http://localhost:' + config.app.port + '/api/users/' + (message.userId || ''),
+                    method: httpActions.user[message.action],
+                    body: JSON.stringify(message.data)
+                }, (val, raw, result) => {
+                    socket.send(JSON.stringify({ type: 'userActionResponse', result: JSON.parse(result)}));
+                });
+            } else {
+                return request({
+                    headers: {'content-type' : 'application/json'},
+                    url: 'http://localhost:' + config.app.port + '/api/projects/' + (message.action == 'getByUserId' ? `by-user/${message.userId}` : (message.projectId || '')),
+                    method: httpActions.project[message.action],
+                    body: JSON.stringify(message.data)
+                }, (val, raw, result) => {
+                    socket.send(JSON.stringify({ type: 'projectActionResponse', result: JSON.parse(result)}));
+                });
+            }
         });
     });
 }
